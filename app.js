09724e2e1f2dced1646a02544eb70acfcf1c9a09
coupapaula <coupa.paula@gmail.com>
2020-08-15 00:21:46 +0800

    first commit

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var port = process.env.PORT || 5000;
 application.use(bodyParser.json())
 application.use(express.urlencoded({ extended: true }));
 
+application.get( '/', (req, res) => res.send ('Hello World !!'));
 application.listen(port, () => {
     console.log('Server is running on port 5000')
 });
